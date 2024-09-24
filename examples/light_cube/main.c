ac1910d7ed0309ff9b8bc852303b8af3ce0b2f1c
Isikcan Yilmaz <can158@gmail.com>
2024-09-25 00:05:40 +0200

    updates

--- examples/light_cube/main.c
+++ examples/light_cube/main.c
@@ -12,27 +12,6 @@
 
 char blink_threadStack[THREAD_STACKSIZE_DEFAULT];
 
-// static void delay(void)
-// {
-//     if (IS_USED(MODULE_ZTIMER)) {
-//         ztimer_sleep(ZTIMER_USEC, 1 * US_PER_SEC);
-//     }
-//     else {
-//         /*
-//          * As fallback for freshly ported boards with no timer drivers written
-//          * yet, we just use the CPU to delay execution and assume that roughly
-//          * 20 CPU cycles are spend per loop iteration.
-//          *
-//          * Note that the volatile qualifier disables compiler optimizations for
-//          * all accesses to the counter variable. Without volatile, modern
-//          * compilers would detect that the loop is only wasting CPU cycles and
-//          * optimize it out - but here the wasting of CPU cycles is desired.
-//          */
-//         uint32_t loops = coreclk() / 20;
-//         for (volatile uint32_t i = 0; i < loops; i++) { }
-//     }
-// }
-
 bool on = false;
 
 void *blink_threadHandler(void *arg)
@@ -50,7 +29,7 @@ void *blink_threadHandler(void *arg)
 			LED0_OFF;
 		}
 		on = !on;
-		printf("TEST %d thread %s\n\b", on, thisThreadName);
+		printf("TEST %d thread %s\r\n", on, thisThreadName);
 		ztimer_sleep(ZTIMER_USEC, 1 * US_PER_SEC);
 	}
 
@@ -67,6 +46,7 @@ int main(void)
 		NULL,
 		"blink_thread"
 	);
+
 	for (;;) {
 		printf("Main created blink thread %d\r\n", blink_threadId);
 		ztimer_sleep(ZTIMER_USEC, 2 * US_PER_SEC);
